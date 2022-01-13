e20dcb11f2918cdc41bd343b85061d7ec861914f
Ritik Rawat <75121026+ritikrawat2447@users.noreply.github.com>
2022-01-13 11:31:07 +0530

    commited

--- app/src/main/java/com/example/android/devprofile/MainActivity.kt
+++ app/src/main/java/com/example/android/devprofile/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.android.devprofile
 
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
+import com.example.android.devprofile.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val bitmap = BitmapFactory.decodeResource(resources , R.drawable.devslopesprofilelogo)
+        val rounded = RoundedBitmapDrawableFactory.create(resources,bitmap)
+        rounded.cornerRadius = 15f
+        binding.logo.setImageDrawable(rounded)
+
     }
 }
